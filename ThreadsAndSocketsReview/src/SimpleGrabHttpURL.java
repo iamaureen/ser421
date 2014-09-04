8564780e4af7dfa32ae07f0015e22f8ea11190fe
Kevin Gary <kgary@asu.edu>
2014-09-04 09:48:44 -0700

    Added simple loop to output HTTP headers

--- ThreadsAndSocketsReview/src/SimpleGrabHttpURL.java
+++ ThreadsAndSocketsReview/src/SimpleGrabHttpURL.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.util.List;
+import java.util.Map;
 import java.io.*;
 
 public class SimpleGrabHttpURL {
@@ -12,7 +14,11 @@ public class SimpleGrabHttpURL {
 		BufferedReader instream = null;
 		try {
 			conn = (HttpURLConnection) new URL(args[0]).openConnection();
-
+			Map<String, List<String>> map = conn.getHeaderFields();
+			for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+				System.out.println("Key : " + entry.getKey() + 
+		                 " ,Value : " + entry.getValue());
+			}
 			instream = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String line = null;
 			while ((line = instream.readLine()) != null) {
